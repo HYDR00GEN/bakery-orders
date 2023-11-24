39e065bea0bb4351175a77a219cd822500d32d24
Ovidiu Paunas <opaunas@e4r5p1.42roma.it>
2023-11-24 19:31:28 +0100

    init

---
+++ src/main/java/it/ovi/ordiniPastery/OrdiniPasteryApplication.java
@@ -0,0 +1,13 @@
+package it.ovi.ordiniPastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdiniPasteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdiniPasteryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/ovi/ordiniPastery/OrdiniPasteryApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ovi.ordiniPastery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdiniPasteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
